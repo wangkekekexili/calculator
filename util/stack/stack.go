56fb9ba9728980116840c0f47307cbc474546c2e
Ke Wang <wangkekekexili@gmail.com>
2016-12-11 23:44:30 -0500

    Update stack.

--- util/stack/stack.go
+++ util/stack/stack.go
@@ -8,8 +8,11 @@ func New() *stack {
 	return &stack{values: make([]interface{}, 0)}
 }
 
-func (stack *stack) Push(v interface{}) {
-	stack.values = append(stack.values, v)
+func (stack *stack) Peek() interface{} {
+	if len(stack.values) == 0 {
+		return nil
+	}
+	return stack.values[len(stack.values)-1]
 }
 
 func (stack *stack) Pop() interface{} {
@@ -19,4 +22,12 @@ func (stack *stack) Pop() interface{} {
 	v := stack.values[len(stack.values)-1]
 	stack.values = stack.values[:len(stack.values)-1]
 	return v
+}
+
+func (stack *stack) Push(v interface{}) {
+	stack.values = append(stack.values, v)
+}
+
+func (stack *stack) Size() int {
+	return len(stack.values)
 }
